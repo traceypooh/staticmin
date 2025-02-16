2ccb1d772c55dae888ec944a7ce0224ae6e93ba1
Tracey Jaquith <tracey@archive.org>
2025-02-15 23:08:25 -0800

    dev testing aid

--- netlify/edge-functions/transformer.js
+++ netlify/edge-functions/transformer.js
@@ -3,7 +3,7 @@ import StaticMan from '../../lib/Staticman.js'
 // eslint-disable-next-line no-console
 const log = console.log.bind(console)
 
-// eslint-disable-next-line consistent-return
+// eslint-disable-next-line consistent-return, no-unused-vars
 export default async (request, context) => {
   // Look for required query parameters
   const url = new URL(request.url)
@@ -13,10 +13,11 @@ export default async (request, context) => {
       sp.get('repository')?.match(/^[a-z0-9_-]+$/i)) {
     // Do a minor origin change, for testing this repo, via `ntl dev -p9999`,
     // against a static blog running on and posting from localhost:8888
-    // const origin = (context.url?.origin ?? '').replace(/:9999$/, ':8888')
+    const cors_origin = (url.origin ?? '*').replace(/:9999$/, ':8888')
+
     const headers = {
       // 'Access-Control-Allow-Origin': origin,
-      'Access-Control-Allow-Origin': request.method === 'OPTIONS' ? '*' : (url.origin ?? '*'),
+      'Access-Control-Allow-Origin': request.method === 'OPTIONS' ? '*' : cors_origin,
       'Access-Control-Allow-Headers': 'Content-Type, X-Requested-With',
       'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
     }
